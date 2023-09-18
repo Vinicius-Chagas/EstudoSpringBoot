1e59cc035e77b7e8bcc869c396191ae216e81377
Vinicius-Chagas <vinicandido00@gmail.com>
2023-09-18 12:13:24 -0300

    Inicializando repositório

---
+++ src/main/java/med/voll/EstudoSpringBoot/EstudoSpringBootApplication.java
@@ -0,0 +1,14 @@
+package med.voll.EstudoSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpringBootApplication.class, args);
+
+	}
+
+}
