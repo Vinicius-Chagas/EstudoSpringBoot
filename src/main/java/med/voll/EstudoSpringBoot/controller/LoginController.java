5a1697ca46caab0dac3c4c40e342e1b27a3fcba9
Vinicius-Chagas <vinicandido00@gmail.com>
2023-10-02 20:22:34 -0300

    WORKING WITH AUTHENICATION, IMPLEMENTING JWT

--- src/main/java/med/voll/EstudoSpringBoot/controller/LoginController.java
+++ src/main/java/med/voll/EstudoSpringBoot/controller/LoginController.java
@@ -2,7 +2,9 @@ package med.voll.EstudoSpringBoot.controller;
 
 import jakarta.validation.Valid;
 import med.voll.EstudoSpringBoot.domain.usuario.DadosAutenticacao;
+import med.voll.EstudoSpringBoot.domain.usuario.Usuario;
 import med.voll.EstudoSpringBoot.domain.usuario.UsuarioRepository;
+import med.voll.EstudoSpringBoot.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,12 +21,15 @@ public class LoginController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid DadosAutenticacao dados){
         var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(tokenService.gerarToken((Usuario) authentication.getPrincipal()));
     }
 
 
