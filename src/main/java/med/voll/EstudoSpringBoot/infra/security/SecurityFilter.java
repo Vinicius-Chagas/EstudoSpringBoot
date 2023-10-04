d31d8d77e786c75f533b6f1f613c3b4079e6c14b
Vinicius-Chagas <vinicandido00@gmail.com>
2023-10-04 20:08:01 -0300

    FINISHING USER AUTHENTICATION

--- src/main/java/med/voll/EstudoSpringBoot/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/EstudoSpringBoot/infra/security/SecurityFilter.java
@@ -3,6 +3,10 @@ package med.voll.EstudoSpringBoot.infra.security;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.EstudoSpringBoot.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -11,8 +15,30 @@ import java.io.IOException;
 @Component
 public class SecurityFilter extends OncePerRequestFilter { //Filter do Spring boot
 
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UsuarioRepository repository;
+
     @Override //Filtro chamado uma vez por requisição
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJwt = recuperarToken(request);
+        if(tokenJwt != null) {
+            var subject = tokenService.getSubject(tokenJwt);
+            var usuario = repository.findByLogin(subject);
+            var authentication = new UsernamePasswordAuthenticationToken(usuario,null,usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            //Pegar o token do cabeçalho, buscar o usuario no banco de dados e em seguida, forçar a authentication do usuario antes de continuar.
+        }
         filterChain.doFilter(request,response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+        if(authorizationHeader != null){
+            return authorizationHeader.replace("Bearer ", "");
+        }
+        return null;
     }
 }
--- src/main/java/med/voll/EstudoSpringBoot/infra/security/secutiryCofigurations.java
+++ src/main/java/med/voll/EstudoSpringBoot/infra/security/secutiryCofigurations.java
@@ -1,25 +1,36 @@
 package med.voll.EstudoSpringBoot.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class secutiryCofigurations {
+    @Autowired
+
+    private SecurityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         return http.csrf(crsf -> crsf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .build(); // Desabilita o Statefull e define Stateless como padrão
+                .authorizeHttpRequests(auth -> auth.requestMatchers(HttpMethod.POST,"/login")
+                        .permitAll()
+                        .anyRequest().authenticated())
+                        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class).build();
+        // Desabilita o Statefull e define Stateless como padrão, não restringir requests sem token para login e definir filtro que vem antes
     }
 
     @Bean
